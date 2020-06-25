b96749fbd16989da81a72a75809d7abba6905a26
blondycage <yakson500@gmail.com>
2020-06-25 11:26:02 -0700

    fhkf

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,15 +2,7 @@ import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
-const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: "1:608477333385:web:69a2a8ad981a8100924358"
-};
+
  const firebaseConfig = {
   apiKey: "AIzaSyA7CqzexNlgZmoLaI_yk8LvwSJ9_qmCZ-Q",
   authDomain: "halalfinans.firebaseapp.com",
