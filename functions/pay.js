d5bf84c9dd0f258ed53c8c2e383d7a0b31aa1707
blondycage <yakson500@gmail.com>
2020-07-13 03:30:17 -0700

    done

--- functions/pay.js
+++ functions/pay.js
@@ -1,42 +1,48 @@
-import fetch from 'node-fetch';
+import axios from 'axios';
 
 export function handler(event, context, callback) {
+    const headers = {
+        'Access-Control-Allow-Headers': "Authorization",
+      "Accept":'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer FLWSECK_TEST-d5d039c7cfa21ed2a6838f6ca1514e5d-X',
+        
+      }
+      
+  axios(
+    {
+      method: 'post',
+      url: 'https://api.flutterwave.com/v3/payments',
+      data:{ tx_ref: 'appppp-tx-19vcvn690878ki77645tty',
+      amount: '10000',
+      currency: 'NGN',
+      redirect_url: 'https://halalfinans.web.app/',
+      payment_options: 'card',
+      meta: {
+        consumer_id: 23,
+        consumer_mac: '92a3-912ba-1192a',
+      },
+      customer: {
+        email: 'yakson500@gmail.com',
+        phonenumber: '080****4528',
+        name: 'Yemi Desola',
+      },
+      customizations: {
+        title: 'Murabahah Payments',
+        description: 'Onetime Pay the price',
+        logo: 'https://assets.piedpiper.com/logo.png',
+      },},
+      headers
+     
+    },
    
-          const requestOptions = {
-            method: 'POST',
-            withCredentials: true,
-            credentials: 'include',
-            headers: {
-              Authorization: 'Bearer FLWSECK_TEST-d5d039c7cfa21ed2a6838f6ca1514e5d-X',
-              'Content-Type': 'application/json',
-            },
-            body: {
-              tx_ref: 'appppp-tx-196878ki77645tty',
-              amount: '10000',
-              currency: 'NGN',
-              redirect_url: 'https://halalfinans.web.app/',
-              payment_options: 'card',
-              meta: {
-                consumer_id: 23,
-                consumer_mac: '92a3-912ba-1192a',
-              },
-              customer: {
-                email: 'yakson500@gmail.com',
-                phonenumber: '080****4528',
-                name: 'Yemi Desola',
-              },
-              customizations: {
-                title: 'Murabahah Payments',
-                description: 'Onetime Pay the price',
-                logo: 'https://assets.piedpiper.com/logo.png',
-              },
-            },
-          };
-          fetch('https://api.flutterwave.com/v3/payments', requestOptions)
-            .then((response) => response.json())
-            .then((data) => {
-              console.log(data);
-            })
-            .catch((err) => console.log(err));
-    
+  )
+    .then((response) => {
+      console.log(response.data);
+      callback(null, {
+        statusCode: 200,
+        body: 'yay',
+      });
+    })
+    .catch((err) => console.log(err));
 }
