c109f5c75db47fc54cd7194bcf299fe5ee5f6382
blondycage <yakson500@gmail.com>
2020-07-14 03:05:23 -0700

    ds

--- functions/pay.js
+++ functions/pay.js
@@ -6,12 +6,14 @@ export function handler(event, context, callback) {
       "Accept":'application/json',
         'Content-Type': 'application/json',
         'Authorization': 'Bearer FLWSECK_TEST-d5d039c7cfa21ed2a6838f6ca1514e5d-X',
+       " Access-Control-Allow-Origin":"*"
         
       }
     const randtx=uuidv1();
   axios(
     {
       method: 'post',
+   
       url:'https://api.flutterwave.com/v3/payments',
       data:{ tx_ref: `MRBTX- ${randtx}`,
       amount: '10000',
